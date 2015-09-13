801e55f1f03f2a76143d2d762a1aff952059ccd5
nthnjustice <n.justice@outlook.com>
2015-09-13 13:49:54 -0400

    Added more to the tutorial tab

--- website/server.R
+++ website/server.R
@@ -55,35 +55,56 @@ shinyServer(function(input, output, session){
   output$stateVariableToggle1 <- renderImage({
     list(src="stateVariableToggle1.png",
          contentType="image/png",
-         alt="A screenshot of the sidebar panel."
+         alt="A screenshot of the state variable toggle buttons."
     )
   }, deleteFile=FALSE)
 
   output$stateVariableToggle2 <- renderImage({
     list(src="stateVariableToggle2.png",
          contentType="image/png",
-         alt="A screenshot of the sidebar panel."
+         alt="A screenshot of the state variable toggle buttons."
     )
   }, deleteFile=FALSE)
 
   output$stateVariableToggle3 <- renderImage({
     list(src="stateVariableToggle3.png",
          contentType="image/png",
-         alt="A screenshot of the sidebar panel."
+         alt="A screenshot of the state variable toggle buttons."
     )
   }, deleteFile=FALSE)
 
   output$dataTableTabScreenshot <- renderImage({
     list(src="dataTableTabScreenshot.png",
          contentType="image/png",
-         alt="A screenshot of the sidebar panel."
+         alt="A screenshot of the data table tab."
     )
   }, deleteFile=FALSE)
 
   output$modelTabScreenshot <- renderImage({
     list(src="modelTabScreenshot.png",
          contentType="image/png",
-         alt="A screenshot of the sidebar panel."
+         alt="A screenshot of the model tab."
+    )
+  }, deleteFile=FALSE)
+
+  output$stateVariableSelectionScreenshot <- renderImage({
+    list(src="stateVariableSelectionScreenshot.png",
+         contentType="image/png",
+         alt="A screenshot of the state variable dropdown."
+    )
+  }, deleteFile=FALSE)
+
+  output$decompositionSelectionScreenshot <- renderImage({
+    list(src="decompositionSelectionScreenshot.png",
+         contentType="image/png",
+         alt="A screenshot of the decomposition dropdown."
+    )
+  }, deleteFile=FALSE)
+
+  output$drawnBreakpointLineScreenshot <- renderImage({
+    list(src="drawnBreakpointLineScreenshot.png",
+         contentType="image/png",
+         alt="A screenshot of drawn breakpoint lines."
     )
   }, deleteFile=FALSE)
 
