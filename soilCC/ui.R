a42059bf6f1b310139bf9e18ee71dd18e57ae0d9
Nathan Justice <n.justice@outlook.com>
2016-01-08 16:33:14 -0600

    updated ui

--- soilCC/ui.R
+++ soilCC/ui.R
@@ -1,4 +1,4 @@
-dashboardPage(skin="green",
+dashboardPage(skin="yellow",
 
   dashboardHeader(title="Costa Rica"),
 
@@ -6,42 +6,56 @@ dashboardPage(skin="green",
     sidebarMenu(
       menuItem("Introduction", tabName="Introduction", icon=icon("home")),
       menuItem("Methodology", tabName="Methodology", icon=icon("book")),
-      menuItem("Plant Key", tabName="Plant Key", icon=icon("tags")),
+      menuItem("Plant Key", tabName="PlantKey", icon=icon("tags")),
       menuItem("Results", tabName="Results", icon=icon("film"))
-    )
-  ),
+    ) # sidebarMenu
+  ), # dashboardSidebar
 
   dashboardBody(
     tabItems(
+
       tabItem(tabName="Introduction",
         fluidRow(
           column(width=12,
-            h1(strong("Soil Chemistry and its Relationship to Understory Plant Community Composition in Costa Rica"))
-          )
-        )
-      ),
+            h1(strong("Soil Chemistry and its Relationship to Understory Plant Community Composition in Costa Rica")),
+            hr()
+          ) # column
+        ) # fluidRow
+      ), # tabItem - Introduction
+
       tabItem(tabName="Methodology",
         fluidRow(
           column(width=12,
                  h1(strong("Content Here"))
-          )
-        )
-      ),
-      tabItem(tabName="Plant Key",
+          ) # column
+        ) # fluidRow
+      ), # tabItem - Methodology
+
+      tabItem(tabName="PlantKey",
         fluidRow(
           column(width=12,
-                 h1(strong("Content Here"))
-          )
-        )
-      ),
+            h1(strong("Content Here"))
+          ) # column
+        ) # fluidRow
+      ), # tabItem - PlantKey
+
       tabItem(tabName="Results",
         fluidRow(
           column(width=12,
-                 h1(strong("Content Here"))
-          )
-        )
+            tabsetPanel(
+              tabPanel(title="Plots",
+                fluidRow(
+                  column(12,
+                    h1(strong("Content Here"))
+                  ) # column
+                ) # fluidRow
+              ) #tabPanel - Plots
+            ) # tabsetPanel
+          ) # column
+        ) # fluidRow
       )
-    )
-  )
 
-)
+    ) # tabItems
+  ) # dashboardBody
+
+) # dashboardPage
